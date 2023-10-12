c2e41cfc4a2aaf7089f129492dcb0fbbd84310e1
kuschem <kuschem@outlook.de>
2023-10-12 17:06:46 +0200

    20231013i

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -6,7 +6,7 @@ add_dateparts as (
         select  *,
                 date_part('year', date) AS year,
                 date_part('month', date) AS month,
-                date_part('week', date) AS weekday,
+                date_part('dow', date) AS weekday,
                 date_part('day', date) AS day_num
         from extracted_temp
 )
