44edd84c0af6d3535848176baa68261c937bb45f
kuschem <kuschem@outlook.de>
2023-10-12 15:33:33 +0200

    20131013a

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -1,7 +1,6 @@
 with extracted_temp as (
     SELECT * 
-        ----extra column creation---
-    from {{ref('staging_weather')}}
+    FROM {{ref('staging_weather')}}
 )
 select *
 from extracted_temp
