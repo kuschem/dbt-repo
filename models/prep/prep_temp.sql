d6799374cd6198c0dbe63d0608733abeb72a82e2
kuschem <kuschem@outlook.de>
2023-10-12 17:02:38 +0200

    20231013h

--- models/prep/prep_temp.sql
+++ models/prep/prep_temp.sql
@@ -1,6 +1,14 @@
 with extracted_temp as (
     SELECT *
     FROM {{ref('staging_weather')}}
+),
+add_dateparts as (
+        select  *,
+                date_part('year', date) AS year,
+                date_part('month', date) AS month,
+                date_part('week', date) AS weekday,
+                date_part('day', date) AS day_num
+        from extracted_temp
 )
-select *
-from extracted_temp
+SELECT *
+FROM add_dateparts
